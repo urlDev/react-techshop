58e9a916db5ee530f791a83efd65419ee53587b2
Can Ural <can.ural@outlook.com>
2019-12-31 17:21:24 +0300

    responsiveness added to navbar

--- src/Components/Navbar/NavTop.jsx
+++ src/Components/Navbar/NavTop.jsx
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import {  Navbar, Nav , Image} from 'react-bootstrap';
+import { Navbar, Nav, Image } from 'react-bootstrap';
 
 import './NavTop.scss';
 
 class NavTop extends Component {
 	render() {
 		return (
-			
-				<Navbar bg="dark" variant="dark">
-					<Navbar.Brand href="#home"><Image src={require("./logo_transparent.png")}/></Navbar.Brand>
-					<Nav className="ml-auto mr-5">
-						<Nav.Link href="#home" className="pr-5">Home</Nav.Link>
-						<Nav.Link href="#features" className="pr-5">Sign In</Nav.Link>
+			<Navbar default collapseOnSelect expand="lg" bg="dark" variant="dark">
+				<Navbar.Brand href="#home">
+					<Image src={require('./logo_transparent.png')} />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav">
+					<i class="fas fa-bars" />
+				</Navbar.Toggle>
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto mr-5 ">
+						<Nav.Link href="#home" className="pr-5">
+							Home
+						</Nav.Link>
+						<Nav.Link href="#features" className="pr-5">
+							Sign In
+						</Nav.Link>
 						<Nav.Link href="#pricing">Cart</Nav.Link>
 					</Nav>
-				</Navbar>
-	
+				</Navbar.Collapse>
+			</Navbar>
 		);
 	}
 }
