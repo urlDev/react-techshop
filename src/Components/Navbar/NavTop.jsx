0696e2e1a5c35916f0cd089c380d3a8236034b01
Can Ural <can.ural@outlook.com>
2020-01-04 21:41:56 +0300

    state shared with navbar, added an animation for items added :rocket:

--- src/Components/Navbar/NavTop.jsx
+++ src/Components/Navbar/NavTop.jsx
@@ -7,30 +7,34 @@ import './NavTop.scss';
 class NavTop extends Component {
 	render() {
 		return (
-			<Navbar default collapseOnSelect expand="lg" bg="dark" variant="dark">
-				<Navbar.Brand as={Link} to="/">
-					<Image src={require('./logo_transparent.png')} />
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav">
-					<i className="fas fa-bars" />
-				</Navbar.Toggle>
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="ml-auto mr-5 ">
-						<Link to="/" className="nav-link pr-5">
-							Home
-						</Link>
-						<Link to="/signin" className="nav-link pr-5">
-							Sign In
-						</Link>
-						
-							<Link to="/cart" className="nav-link">
-								Cart
+			<ProductConsumer>
+			{value => {
+				return (
+					<Navbar default collapseOnSelect expand="lg" bg="dark" variant="dark">
+					<Navbar.Brand as={Link} to="/">
+						<Image src={require('./logo_transparent.png')} />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav">
+						<i className="fas fa-bars" />
+					</Navbar.Toggle>
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ml-auto mr-5 ">
+							<Link to="/" className="nav-link pr-5">
+								Home
 							</Link>
-							
-						
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
+							<Link to="/signin" className="nav-link pr-5">
+								Sign In
+							</Link>
+							<Link to="/cart" className="nav-link">
+								{value.cart.length >= 1 ? (<h3 className="text-primary shake"><i className="fas fa-cart-plus" /> {value.cart.length}</h3>) : (<h3><i className="fas fa-cart-plus" /> {value.cart.length}</h3>) }
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+				)
+			}}
+				
+			</ProductConsumer>
 		);
 	}
 }
